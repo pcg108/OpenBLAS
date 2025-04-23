bf30d20969b408aa3a108c2b4dab92b32000e911
zekailin00 <zekailin00@gmail.com>
2025-04-22 21:50:57 -0700

    gemmini headers

---
+++ gemmini/character.h
@@ -0,0 +1,10 @@
+// See LICENSE for license details.
+
+#ifndef SRC_MAIN_C_CHARACTER_H
+#define SRC_MAIN_C_CHARACTER_H
+
+#include "./xcustom.h"
+
+#define XCUSTOM_CHAR 2
+
+#endif  // SRC_MAIN_C_CHARACTER_H
