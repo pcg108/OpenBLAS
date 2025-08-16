2584103ed1aadcfbd6b7b6a0db08751cfa3014d5
Prashanth Ganesh <prashanthcganesh108@illixr.CS.Berkeley.EDU>
2025-08-15 20:45:23 -0700

    adding rerocc

--- interface/gemv.c
+++ interface/gemv.c
@@ -275,12 +275,12 @@ void CNAME(enum CBLAS_ORDER order,
     ENSURE_GEMMINI_SEMAPHORE();
     sem_wait(gemmini_sem);
 
-    // int acquired = 0;
-    // do {
-    //   acquired = rr_acquire_single(1, 1); // acquire accelerator 1 (fp32 in RocketDualGemminiConfig), and set it to config register 1
-    // } while (acquired == 0);
+    int acquired = 0;
+    do {
+      acquired = rr_acquire_single(1, 1); // acquire accelerator 1 (fp32 in RocketDualGemminiConfig), and set it to config register 1
+    } while (acquired == 0);
 
-    // rr_set_opc(XCUSTOM_ACC, 1); // once the accelerator is acquired, route the command to accelerator 1 (the fp32 gemmini)
+    rr_set_opc(XCUSTOM_ACC, 1); // once the accelerator is acquired, route the command to accelerator 1 (the fp32 gemmini)
 
 #if !defined(DOUBLE)
     gemmini_flush(0);
@@ -346,7 +346,7 @@ void CNAME(enum CBLAS_ORDER order,
     free(x_buf);
     free(y_buf);
 
-    // rr_release(1); // release the accelerator
+    rr_release(1); // release the accelerator
     sem_post(gemmini_sem);
 
 #endif
